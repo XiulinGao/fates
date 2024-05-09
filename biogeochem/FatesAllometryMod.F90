ea2932d602ca196a012a26de226c0c055f370f15
Xiulin Gao <xiulin.gao.edu@gmail.com>
2024-05-09 12:17:55 -0600

    crown area allometry bug fix

--- biogeochem/FatesAllometryMod.F90
+++ biogeochem/FatesAllometryMod.F90
@@ -539,6 +539,16 @@ contains
           call carea_2pwr(dbh_eff,site_spread,d2bl_p2,d2bl_ediff,d2ca_min,d2ca_max, &
                crowndamage, c_area, do_inverse)
           capped_allom = .true.
+       case (4)
+          dbh_eff = min(dbh,dbh_maxh)
+          call h_allom(dbh,ipft,height)
+          call carea_3pwr(dbh_eff,height,ipft,dbh_maxh, site_spread,d2bl_p2, &
+               d2bl_ediff, d2ca_min,d2ca_max,crowndamage, c_area, do_inverse)
+          capped_allom = .true.
+       case(5)
+          call carea_2pwr(dbh,site_spread,d2bl_p2,d2bl_ediff,d2ca_min,d2ca_max, &
+               crowndamage, c_area, do_inverse)
+          capped_allom = .false.
        case DEFAULT
           write(fates_log(),*) 'An undefined leaf allometry was specified: ', &
                allom_lmode
