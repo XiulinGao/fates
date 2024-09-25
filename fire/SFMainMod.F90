bcea284487b4b9830852bde6aee76836d0ff0bd4
Xiulin Gao <xiulin.gao.edu@gmail.com>
2024-09-25 15:21:34 -0700

    fix typo

--- fire/SFMainMod.F90
+++ fire/SFMainMod.F90
@@ -1147,7 +1147,7 @@ contains
             currentPatch%rxfire_frac_burnt = currentPatch%area / total_burnable_area * &
             min(0.99_r8, (SF_val_rxfire_AB / bc_in%site_area) !we cap fractional burning capacity in case site area is too small if that can be true?
          else
-            currentPatch%rxfire == 0 !update rxfire tag when fraction burnable area is less then 50% of grid area, so we do not apply rx fire  
+            currentPatch%rxfire = 0 !update rxfire tag when fraction burnable area is less then 50% of grid area, so we do not apply rx fire  
             currentPatch%rxfire_frac_burnt = 0.0_r8  
             !currentPatch%rxfire_FI = 0.0_r8
          endif
