f31ce8ed17e751b534f3606b4ed6feacf308d03f
Xiulin Gao <xiulin.gao.edu@gmail.com>
2024-10-13 11:03:57 -0700

    add crown fire switch

--- main/EDParamsMod.F90
+++ main/EDParamsMod.F90
@@ -77,8 +77,8 @@ module EDParamsMod
                                                                       ! (3) for the Tree Recruitment Scheme without seedling dynamics
    
    
-   logical,protected, public :: active_crown_fire        ! flag, 1=active crown fire 0=no active crown fire
-   character(len=param_string_length),parameter :: fates_name_active_crown_fire = "fates_fire_active_crown_fire"
+   integer,protected, public :: crown_fire_switch        ! flag, 1=active crown fire 0=no active crown fire
+   
 
    real(r8), protected, public :: cg_strikes             ! fraction of cloud to ground lightning strikes (0-1)
    character(len=param_string_length),parameter :: fates_name_cg_strikes="fates_fire_cg_strikes"
@@ -184,6 +184,7 @@ integer, parameter, public :: maxpft = 16      ! maximum number of PFTs allowed
    character(len=param_string_length),parameter,public :: ED_name_canopy_closure_thresh= "fates_canopy_closure_thresh"      
    character(len=param_string_length),parameter,public :: ED_name_stomatal_model= "fates_leaf_stomatal_model"
    character(len=param_string_length),parameter,public :: ED_name_regeneration_model= "fates_regeneration_model"
+   character(len=param_string_length),parameter,public :: fates_name_crown_fire_switch = "fates_crown_fire_switch"
 
    character(len=param_string_length),parameter,public :: name_theta_cj_c3 = "fates_leaf_theta_cj_c3"
    character(len=param_string_length),parameter,public :: name_theta_cj_c4 = "fates_leaf_theta_cj_c4"
@@ -605,7 +606,7 @@ contains
     call fates_params%RegisterParameter(name=ED_name_history_height_bin_edges, dimension_shape=dimension_shape_1d, &
          dimension_names=dim_names_height)
 
-    call fates_params%RegisterParameter(name=fates_name_active_crown_fire, dimension_shape=dimension_shape_scalar, &
+    call fates_params%RegisterParameter(name=fates_name_crown_fire_switch, dimension_shape=dimension_shape_scalar, &
          dimension_names=dim_names_scalar)
     
     call fates_params%RegisterParameter(name=fates_name_cg_strikes, dimension_shape=dimension_shape_scalar, &
@@ -815,9 +816,9 @@ contains
     call fates_params%RetrieveParameter(name=name_dev_arbitrary, &
          data=dev_arbitrary)
 
-    call fates_params%RetrieveParameter(name=fates_name_active_crown_fire, & 
+    call fates_params%RetrieveParameter(name=fates_name_crown_fire_switch, & 
           data=tmpreal)
-    active_crown_fire = (abs(tmpreal-1.0_r8)<nearzero)
+    crown_fire_switch = nint(tmpreal)
 
     call fates_params%RetrieveParameter(name=fates_name_cg_strikes, &
           data=cg_strikes)
@@ -918,7 +919,7 @@ contains
         write(fates_log(),fmt0) 'q10_mr = ',q10_mr
         write(fates_log(),fmt0) 'q10_froz = ',q10_froz
         write(fates_log(),fmt0) 'cg_strikes = ',cg_strikes
-        write(fates_log(),'(a,L2)') 'active_crown_fire = ',active_crown_fire
+        write(fates_log(),fmt0) 'crown_fire_switch = ',crown_fire_switch
         write(fates_log(),fmt0) 'damage_event_code = ',damage_event_code
         write(fates_log(),fmt0) 'damage_canopy_layer_code = ', damage_canopy_layer_code
         write(fates_log(),*) '------------------------------------------------------'
