2574110ac4dda168a0f927f8d6d740ee243a79cb
Xiulin Gao <xiulin.gao.edu@gmail.com>
2024-11-06 16:30:24 -0800

    bug fix

--- biogeochem/EDMortalityFunctionsMod.F90
+++ biogeochem/EDMortalityFunctionsMod.F90
@@ -253,7 +253,7 @@ contains
     integer,          intent(in)               :: anthro_disturbance_label
     real(r8),         intent(in)               :: age_since_anthro_disturbance
     real(r8),         intent(in)               :: frac_site_primary 
-    real(r8),         intent(in)               :: delta_BA
+    real(r8),         intent(inout)            :: delta_BA
     real(r8),         intent(in)               :: area 
 
     real(r8), intent(in) :: harvestable_forest_c(:)   ! total carbon available for logging, kgC site-1
