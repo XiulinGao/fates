d2558de9b3f89d28cc77dc3d3e9e85dcb2537ea5
Xiulin Gao <xiulin.gao.edu@gmail.com>
2025-03-06 14:07:34 -0800

    add rxfire for fuel burnt

--- biogeochem/EDPatchDynamicsMod.F90
+++ biogeochem/EDPatchDynamicsMod.F90
@@ -1826,7 +1826,8 @@ contains
 
        do c = 1,ncwd
          frac_burnt = 0.0_r8
-         if (dist_type == dtype_ifire .and. currentPatch%fire == 1) then
+         if (dist_type == dtype_ifire .and. (currentPatch%fire == 1 .or. &
+         currentPatch%rxfire == 1)) then
             frac_burnt = currentPatch%burnt_frac_litter(c)
          end if 
              
@@ -1853,7 +1854,8 @@ contains
        enddo
 
        frac_burnt = 0.0_r8
-       if (dist_type == dtype_ifire .and. currentPatch%fire == 1) then
+       if (dist_type == dtype_ifire .and. (currentPatch%fire == 1 .or. &
+       currentPatch%rxfire == 1)) then
          frac_burnt = currentPatch%burnt_frac_litter(dl_sf)
       end if 
           
