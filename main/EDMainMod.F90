c7fae069a8a6b0d0b4b9ee66b09b0bb9b4264ed8
Xiulin Gao <xiulin.gao.edu@gmail.com>
2025-03-15 19:40:41 -0700

    bug

--- main/EDMainMod.F90
+++ main/EDMainMod.F90
@@ -480,8 +480,8 @@ contains
              call Mortality_Derivative(currentSite, currentCohort, bc_in,      &
                currentPatch%btran_ft, mean_temp,                               &
                currentPatch%land_use_label,                                    &
-               currentPatch%delta_BA, currentPatch%area,                       &
                currentPatch%age_since_anthro_disturbance,                      &
+               currentPatch%delta_BA, currentPatch%area,                       &
                current_fates_landuse_state_vector,                             &
                  harvestable_forest_c, harvest_tag)
 
