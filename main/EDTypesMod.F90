10ef9631627bd2f63e23214bbf6bb49e602514ca
Xiulin Gao <xiulin.gao.edu@gmail.com>
2026-09-17 17:17:00 -0700

    correct termination carbon flux dimension

--- main/EDTypesMod.F90
+++ main/EDTypesMod.F90
@@ -374,9 +374,9 @@ module EDTypesMod
      real(r8), allocatable :: term_nindivs_ustory(:,:,:) ! number of understory individuals that were in cohorts which 
                                                        ! were terminated this timestep, by ermination type, size x pft
 
-     real(r8), allocatable :: term_carbonflux_canopy(:)  ! carbon flux from live to dead pools associated 
+     real(r8), allocatable :: term_carbonflux_canopy(:,:)  ! carbon flux from live to dead pools associated 
                                                          ! with termination mortality, per canopy level. [kgC/ha/day]
-     real(r8), allocatable :: term_carbonflux_ustory(:)  ! carbon flux from live to dead pools associated 
+     real(r8), allocatable :: term_carbonflux_ustory(:,:)  ! carbon flux from live to dead pools associated 
                                                          ! with termination mortality, per canopy level.  [kgC/ha/day]    
      real(r8), allocatable :: imort_carbonflux(:)        ! biomass of individuals killed due to impact mortality per year. [kgC/m2/sec]
      real(r8), allocatable :: fmort_carbonflux_canopy(:) ! biomass of canopy indivs killed due to fire per year. [gC/m2/sec]
